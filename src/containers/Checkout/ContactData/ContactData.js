a006eb5f740c7935229d28a4675bdd927ebc5495
Kashif Khan <khank@versifit.com>
2020-12-29 16:11:23 +0530

    Add input element for form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,13 +2,66 @@ import React, { Component } from 'react';
 import Button from './../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 import axios from './../../../axiosOrder';
-
+import Loader from './../../../components/UI/Spinner/Spinner';
+import Input from './../../../components/UI/Input/Input';
 class ContactData extends Component {
 
     state = {
         ingredients: null,
         price: 0,
-        isLoading: false
+        isLoading: false,
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' }
+                    ]
+                },
+                value: ''
+            }
+        },
     }
 
     componentDidMount() {
@@ -19,11 +72,6 @@ class ContactData extends Component {
     }
     orderHandler = (event) => {
         event.preventDefault();
-
-        // this.props.history.push({
-        //     pathname: "/checkout",
-        //     search: queryParams.join("&")
-        // });
         const orderData = {
             ingredients: this.state.ingredients,
             price: this.state.totalCost,
@@ -47,29 +95,61 @@ class ContactData extends Component {
                 this.setState({
                     isLoading: false
                 })
+                this.props.history.push("/orders");
             }).catch(error => {
                 console.log(error);
                 this.setState({
                     isLoading: false
                 })
             })
-        alert('continue')
     }
     render() {
-        console.log(this.props.ingredients)
-        console.log(this.props)
 
+        let elementArray = [];
+        for (let element in this.state.orderForm) {
+            elementArray.push({
+                id: element,
+                config: this.state.orderForm[element]
+            })
+        }
+        let form = null;
+        if (this.state.isLoading) {
+            form = <Loader />;
+
+        } else {
+
+            //form =
+
+                form = (
+                    <form>
+                        {elementArray.map(formElement => {
+                            return <Input
+                                key={formElement.id}
+                                elementType={formElement.config.elementType}
+                                elementConfig={formElement.config.elementConfig}
+                                value={formElement.config.value}
+                            />
+                        })}
+                          <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
+                    </form>
+                )
+
+            // form = (
+            //     <form>
+            //         <Input />
+            //         {/* <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+            //         <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+            //         <input className={classes.Input} type="text" name="street" placeholder="Street" />
+            //         <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" /> */}
+            //         <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
+
+            //     </form>
+            // )
+        }
         return (
             <div className={classes.ContactData}>
                 <h1>Contact Details</h1>
-                <form>
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-                <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
-
-                </form>
+                {form}
             </div>
         )
     }
